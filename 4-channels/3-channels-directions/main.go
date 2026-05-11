3122f5d18a8a6a6683d77fe72614fce86c2b677d
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-11 20:30:29 -0300

    channels directions

---
+++ 4-channels/3-channels-directions/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func publisher(name string, ch chan<- string) { //assinatura de canal que so recebe dados
+	ch <- name //canal recebe mensagem
+}
+
+func reader(ch <-chan string) { // assinatura de canal que entrega dados
+	fmt.Println(<- ch)
+}
+
+// thread 1
+func main() {
+	myChannel := make(chan string)
+
+	go publisher("Hello", myChannel)
+	reader(myChannel)
+}
